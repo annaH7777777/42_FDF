61b45fe91315b1585d34a932bb6c4c2ae505d0d6
Anna <anna.noorgames@gmail.com>
2022-01-04 20:43:31 +0400

    added drawing

--- fdf.h
+++ fdf.h
@@ -14,11 +14,17 @@ typedef struct
     int width;
     int height;
     int **z_matrix;
+    int zoom;
+    int color;
+    int shift_x;
+    int shift_y;
 
     void *mlx_ptr;
     void *win_ptr;
 }   fdf;
 
 void read_file(char *file_name, fdf *data);
+void bresenham(float x, float y, float x1, float y1, fdf *data);
+void draw(fdf *data);
 
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include "fdf.h"
 
-int deal_key(int key, void *data)
+int deal_key(int key, fdf *data)
 {
     printf("%d", key);
+    if(key == 126)
+        data->shift_y -= 10;
+    else if(key == 125)
+        data->shift_y += 10;
+    else if(key == 123)
+        data->shift_x -= 10;
+    else if(key == 124)
+        data->shift_x += 10;
+    else
+        return;
+    mlx_clear_window(data->mlx_ptr, data->win_ptr);
+    draw(data);
+    
     return (0);
 }
 
@@ -28,4 +41,13 @@ int main(int argc, char **argv)
         printf("\n");
         i++;
     }
+
+    data->mlx_ptr = mlx_init();
+    data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "FDF");
+    data->zoom = 20;
+
+    //bresenham(10, 10, 600, 300, data);
+    draw(data);
+    mlx_key_hook(data->win_ptr, deal_key, data);
+    mlx_loop(data->mlx_ptr);
 }
